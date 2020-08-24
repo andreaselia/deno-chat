cc352e10f9c3dbc77e46fdb708aef1c20a4cfa81
Andreas Elia <andreaselia@live.co.uk>
2020-08-24 23:36:05 +0100

    update

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,11 +13,9 @@ export default {
 
       switch (event.event) {
         case 'message':
-          console.log(123)
           this.$store.messages = [...this.$store.messages, event.message]
           break
         case 'channelChange':
-          console.log(123)
           this.$store.currentChannel = event.channel
           break
       }
